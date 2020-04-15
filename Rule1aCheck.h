83d70a06f2d16db6554664674c2f14556570e8a3
Connor-McMillin <connormcmillin@comcast.net>
2020-04-15 05:37:48 -0400

    Added rule 1A check to all necessary names. This encompasses functions, variables, unions, structs and fields in unions/structs

--- Rule1aCheck.h
+++ Rule1aCheck.h
@@ -25,6 +25,7 @@ public:
       : ClangTidyCheck(Name, Context) {}
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+  void check_name(SourceLocation loc, std::string name, std::string type);
 };
 
 } // namespace eastwood
