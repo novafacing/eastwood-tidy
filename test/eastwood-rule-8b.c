cb4fff4b33e5fb70c7448e6c4692dd483928acaa
novafacing <rowanbhart@gmail.com>
2020-11-29 22:12:43 -0500

    Complete Rule 8b

--- test/eastwood-rule-8b.c
+++ test/eastwood-rule-8b.c
@@ -2,7 +2,11 @@
 /* --------------------- Test Code Style Section VIII.B 
  * Header files should be self-contained (compile on their own) and end in .h.
  * ------------------------------------------------------------------------------------------------------ */
+#include "eastwood-rule-8b.hh"
+#include "eastwood-rule-8b.hpp"
 #include "eastwood-rule-8b.h"
+
+
 
 
 // Put Something That Fails Here
