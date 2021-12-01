7d4247b223d9577920c9d650b0e1ed31059e0472
novafacing <rowanbhart@gmail.com>
2021-12-01 07:21:28 -1000

    Enhance trailing whitespace testcase

--- test/eastwood-rule-2a.c
+++ test/eastwood-rule-2a.c
@@ -7,13 +7,37 @@
  * ---------------------------------------------------------------------------------------------------- */
 #include "eastwood-rule-2a.h"
 
-int foo(void) {
+int foo(void) { 
 	int a = 10; // Bad: there is a tab here instead of spaces
 // CHECK-MESSAGES: :[[@LINE-1]]:[column goes here]:  Failure Message Goes Here
   int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = 10; // Bad: line length
   int b = 10; 
   // Bad, the previous line has a trailing whitespace
-}
+  if (1) { 
+    // Bad, the previous line has a trailing whitespace
+    int c = 10; 
+    // Bad: the previous line has a trailing whitespace
+    while (1) { 
+      // Bad: the previous line has a trailing whitespace
+      int d = 10; 
+      // Bad: the previous line has a trailing whitespace
+      switch (d) { 
+        // Bad: the previous line has a trailing whitespace
+        case 1: 
+          // Bad: the previous line has a trailing whitespace
+          d = 10; 
+          // Bad: the previous line has a trailing whitespace
+          break; 
+        default: { 
+          // Bad: the previous line has a trailing whitespace
+          d = 11; 
+          // Bad: the previous line has a trailing whitespace
+          break; 
+        } 
+      } 
+    } 
+  } 
+} 
 // Put Something That Fails Here
 
 int bar(void) {
