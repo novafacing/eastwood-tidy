28158a770c8f1afbf02a72577f99c13345f6554f
novafacing <rowanbhart@gmail.com>
2023-06-06 22:39:35 -0700

    Start implementation of Rule 9A

--- src/Rule9aCheck.cpp
+++ src/Rule9aCheck.cpp
@@ -8,14 +8,36 @@
 //===----------------------------------------------------------------------===//
 
 #include "Rule9aCheck.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
 
 using namespace clang::ast_matchers;
 
 namespace clang {
 namespace tidy {
 namespace eastwood {
-void Rule9aCheck::registerMatchers(MatchFinder *Finder) {}
-void Rule9aCheck::check(const MatchFinder::MatchResult &Result) {}
+Rule9aCheck::Rule9aCheck(StringRef name, ClangTidyContext *Context)
+    : ClangTidyCheck(name, Context), EastwoodTidyCheckBase(name),
+      debug_enabled(Options.get("debug", "false")) {
+    if (this->debug_enabled == "true") {
+        this->debug = true;
+    }
+}
+void Rule9aCheck::registerMatchers(MatchFinder *Finder) {
+    this->register_relex_matchers(Finder, this);
+    Finder->addMatcher(
+        varDecl(hasInitializer(callExpr(callee(functionDecl(
+                    anyOf(hasName("fopen"), hasName("malloc"), hasName("calloc"),
+                          hasName("fread"), hasName("fwrite"), hasName("system")))))))
+            .bind("var_from_call"),
+        this);
+}
+void Rule9aCheck::check(const MatchFinder::MatchResult &Result) {
+    this->acquire_common(Result);
+    RELEX();
+    if (auto MatchedDecl = Result.Nodes.getNodeAs<VarDecl>("var_from_call")) {
+        // MatchedDecl.
+    }
+}
 } // namespace eastwood
 } // namespace tidy
 } // namespace clang
